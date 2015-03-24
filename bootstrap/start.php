64cb41e4f4cdfb5caeddaf23a6d8107c5c23f41e
Hendy <hendy.yanuar@kiranatama.com>
2015-03-24 11:16:11 +0700

    Set environment for staging and production

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,6 +27,8 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('precise64'),
+	'staging' => array('ip-172-31-0-153'),
+	'production' => array('ip-172-31-4-198'),
 
 ));
 
